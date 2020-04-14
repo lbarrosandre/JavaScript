68368c4711f91bbf2a6bd0f3a6a71c1f75b4788e
André Luiz Barros <lbarros.andre@gmail.com>
2020-04-14 08:58:36 -0300

    Atualização atividades

---
+++ Exercicios/ex014_extra/script.js
@@ -0,0 +1,17 @@
+function carregar() {
+    var msg = window.document.getElementById("msg")
+    var img = window.document.getElementById("imagem")
+    var data = new Date()
+    var hora = data.getHours()
+        msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora < 12) {
+        img.src = 'manha.jpg'
+        document.body.style.background = '#6c63ff'
+    } else if (hora < 18) {
+        img.src = 'tarde.jpg'
+        document.body.style.background = '#f87114'
+    }else {
+        img.src = 'noite.jpg '
+        document.body.style.background = '#353535'
+    }
+}
